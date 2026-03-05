a7cf7e56b829a1a8854ed597e5ddb8f6174a8ef3
OwiesAlhajali <116441812+OwiesAlhajali@users.noreply.github.com>
2026-03-06 01:18:34 +0300

    initial project

---
+++ src/test/java/com/trainingcenter/management/TrainingCenterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingcenter.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingCenterManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
