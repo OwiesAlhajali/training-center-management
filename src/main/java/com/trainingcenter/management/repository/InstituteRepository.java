777343acb7defffd611443ea62ee657e0a7deb9a
Hussien Ramadan <166642945+HussienRamadan1@users.noreply.github.com>
2026-06-25 23:48:24 +0300

    Update InstituteRepository.java

--- src/main/java/com/trainingcenter/management/repository/InstituteRepository.java
+++ src/main/java/com/trainingcenter/management/repository/InstituteRepository.java
@@ -14,4 +14,20 @@ public interface InstituteRepository extends JpaRepository<Institute, Long> {
     
     @Query("SELECT i.tenant.id FROM Institute i WHERE i.id = :instituteId")
     Long findTenantIdByInstituteId(@Param("instituteId") Long instituteId);
+
+    
+@Query("SELECT COUNT(DISTINCT e.student.id) " +
+       "FROM Enrollment e " +
+       "JOIN e.trainingSession ts " +
+       "JOIN ts.classRoom cr " +
+       "WHERE cr.institute.id = :instituteId")
+long countStudentsByInstitute(@Param("instituteId") Long instituteId);
+    
+@Query("SELECT COUNT(DISTINCT t.id) " +
+       "FROM Teacher t " +
+       "JOIN TrainingSession ts ON ts.teacher.id = t.id " +
+       "JOIN ts.classRoom cr " +
+       "WHERE cr.institute.id = :instituteId")
+long countTeachersByInstitute(@Param("instituteId") Long instituteId);
+    
 }
