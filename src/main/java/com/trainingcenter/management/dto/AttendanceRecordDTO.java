f2cf9b3a28613a547db805af14ba05c714955719
OwiesAlhajali <116441812+OwiesAlhajali@users.noreply.github.com>
2026-06-29 23:19:28 +0300

    Refactor attendance service to use upsert and improve validation

--- src/main/java/com/trainingcenter/management/dto/AttendanceRecordDTO.java
+++ src/main/java/com/trainingcenter/management/dto/AttendanceRecordDTO.java
@@ -1,10 +1,15 @@
 package com.trainingcenter.management.dto;
 
 import com.trainingcenter.management.entity.AttendanceStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AttendanceRecordDTO {
+
+    @NotNull(message = "Student ID is required")
     private Long studentId;
+
+    @NotNull(message = "Attendance status is required")
     private AttendanceStatus status;
 }
